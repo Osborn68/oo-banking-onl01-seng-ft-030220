06fe4dcefb02c0aef762faffc162268ff475b06b
Christopher Osborn <z68ozz@gmail.com>
2020-03-20 03:23:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,9 @@ def execute_transaction
    @status = "complete"
    elsif !valid?
    @status = "rejected"
-   
+    "Transaction rejected. Please check your account balance."
+  end
+end
     
   def reverse_transfer
     
