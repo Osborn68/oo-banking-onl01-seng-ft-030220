e46bba28c7096f20b9e2b6c110d06e2db1718857
Christopher Osborn <z68ozz@gmail.com>
2020-03-20 03:17:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,6 +21,7 @@ def execute_transaction
   if @sender.balance < @amount || !valid?
     @status = "rejected"
     "Transaction rejected. Please check your account balance."
+  elsif @status == "complete"
   else
     @sender.deposit(@amount * -1)
     @receiver.deposit(@amount)
