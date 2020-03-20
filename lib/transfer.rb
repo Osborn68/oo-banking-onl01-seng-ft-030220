113e53b942727ff7c5fd1bfc54ecc9335b7eda7e
Christopher Osborn <z68ozz@gmail.com>
2020-03-20 03:25:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,11 @@ def execute_transaction
 end
     
   def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(amount)
+      @receiver.deposit(-amount)
+      @status = "reversed"
+      
     
 
 end
